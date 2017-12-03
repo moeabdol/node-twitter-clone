8286e2503ed7f4ed73b53215aaf34107139aca9e
moeabdol <mohd.a.saed@gmail.com>
2017-12-03 17:51:19 +0300

    Implement method in user model to hash password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('../config/mongoose');
+const bcrypt   = require('bcrypt');
+const crypto   = require('crypto');
+
+const SALT_FACTOR = 10;
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -10,5 +14,28 @@ const UserSchema = new Schema({
     tweet: { type: Schema.Types.ObjectId, ref: 'Tweet' }
   }]
 });
+
+UserSchema.pre('save', function(next) {
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(SALT_FACTOR)
+      .then(salt => {
+        bcrypt.hash(this.password, salt)
+          .then(hash => {
+            this.password = hash;
+            next();
+          })
+          .catch(err => next(err));
+      })
+      .catch(err => next(err));
+  } else {
+    next();
+  }
+});
+
+UserSchema.methods.gravatar = function(size) {
+  if (!size) size = 200;
+  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return `https://gravatar.com/avatar/${md5}?s=${size}$d=retro`;
+};
 
 module.exports = mongoose.model('User', UserSchema);
